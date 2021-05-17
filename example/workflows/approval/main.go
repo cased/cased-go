251d193667275390cdf872188cdef420a0e52d9e
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2021-05-16 21:17:41 -0700

    Finalize approval example

--- example/workflows/approval/main.go
+++ example/workflows/approval/main.go
@@ -13,6 +13,11 @@ import (
 
 const WorkflowName = "provision-database"
 
+var (
+	promptedForAuthentication = false
+	promptedForApproval       = false
+)
+
 func main() {
 	// Fetch the reason required workflow
 	w := fetchWorkflow()
@@ -57,10 +62,11 @@ func createWorkflow() (*cased.Workflow, error) {
 	w, err := workflow.New(&cased.WorkflowParams{
 		Name: cased.String(WorkflowName),
 		Controls: &cased.WorkflowControlsParams{
-			Reason: cased.Bool(true),
+			Authentication: cased.Bool(true),
+			Reason:         cased.Bool(true),
 			Approval: &cased.WorkflowControlsApprovalParams{
 				Count:        cased.Int(1),
-				SelfApproval: cased.Bool(false),
+				SelfApproval: cased.Bool(true),
 				Sources: &cased.WorkflowControlsApprovalSourcesParams{
 					Slack: &cased.WorkflowControlsApprovalSourcesSlackParams{
 						Channel: cased.String("#dewski-guard-test"),
@@ -71,15 +77,7 @@ func createWorkflow() (*cased.Workflow, error) {
 	})
 
 	if err != nil {
-		// Handle errors that came from Cased
-		if casedErr, ok := err.(*cased.Error); ok {
-			switch casedErr.Code {
-			default:
-				return nil, err
-			}
-		} else {
-			return nil, err
-		}
+		return nil, err
 	}
 
 	return w, nil
@@ -96,7 +94,18 @@ func triggerWorkflow(w *cased.Workflow, reason string) {
 		panic(err)
 	}
 
+	fmt.Printf("Created event %s\n", e.ID)
+
 	resolveEvent(w, e)
+}
+
+func refreshEvent(w *cased.Workflow, e *cased.Event) {
+	newEvent, err := event.Get(e.ID)
+	if err != nil {
+		panic(err)
+	}
+
+	resolveEvent(w, newEvent)
 }
 
 func resolveEvent(w *cased.Workflow, e *cased.Event) {
@@ -104,10 +113,6 @@ func resolveEvent(w *cased.Workflow, e *cased.Event) {
 	case cased.WorkflowStateFulfilled:
 		fmt.Println("Workflow successful!")
 	case cased.WorkflowStateUnfulfilled:
-		if e.Result.Controls.Authentication != nil {
-			fmt.Println("fill this out")
-		}
-
 		// The reason control is unfulfilled
 		reason := e.Result.Controls.Reason
 		if reason != nil && reason.State == cased.WorkflowStateUnfulfilled {
@@ -115,16 +120,36 @@ func resolveEvent(w *cased.Workflow, e *cased.Event) {
 			return
 		}
 
+		authentication := e.Result.Controls.Authentication
+		if authentication != nil && authentication.State == cased.WorkflowStateUnfulfilled {
+			authenticationPrompt(w, e, authentication)
+			return
+		}
+
 		// The reason control is unfulfilled
 		approval := e.Result.Controls.Approval
 		if approval != nil {
 			resolveApproval(w, e, approval)
+			return
 		}
 	case cased.WorkflowStateRejected:
 		panic("Workflow rejected")
 	}
 }
 
+// Prompt the user to authenticate with Cased per the configured workflow
+// controls.
+func authenticationPrompt(w *cased.Workflow, e *cased.Event, authentication *cased.ResultControlsAuthentication) {
+	if !promptedForAuthentication {
+		fmt.Printf("To login, please visit:\n%s\n", authentication.URL)
+		promptedForAuthentication = true
+	}
+
+	refreshEvent(w, e)
+}
+
+// Prompt the user for a reason and trigger a new workflow with the provided
+// reason
 func reasonPrompt(w *cased.Workflow, e *cased.Event) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Provide a reason: ")
@@ -136,18 +161,18 @@ func reasonPrompt(w *cased.Workflow, e *cased.Event) {
 	triggerWorkflow(w, strings.TrimSpace(input))
 }
 
+// Handle each approval state
 func resolveApproval(w *cased.Workflow, e *cased.Event, approval *cased.ResultControlsApproval) {
 	switch approval.State {
 	case cased.ResultControlsApprovalStatePending:
-		// Waiting for other controls
+		refreshEvent(w, e)
 	case cased.ResultControlsApprovalStateRequested:
-		fmt.Println("Waiting for approval…")
-		newEvent, err := event.Get(e.ID)
-		if err != nil {
-			panic(err)
+		if !promptedForApproval {
+			fmt.Println("Waiting for approval…")
+			promptedForApproval = true
 		}
 
-		resolveEvent(w, newEvent)
+		refreshEvent(w, e)
 	case cased.ResultControlsApprovalStateApproved:
 		newEvent, err := event.Get(e.ID)
 		if err != nil {
