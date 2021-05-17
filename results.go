d41a548bc6f1d0144b59e2ab958443cf123daaf8
Garrett Bjerkhoel <me@garrettbjerkhoel.com>
2021-05-16 21:18:02 -0700

    Add initial tests

--- results.go
+++ results.go
@@ -7,16 +7,20 @@ type Result struct {
 	// The API URL for the result.
 	ApiURL string `json:"api_url"`
 
-	// State
+	// State contains the workflow run state.
 	State WorkflowState `json:"state"`
 
-	// Controls
+	// Controls contains the controls specified by the workflow that was triggered.
 	Controls ResultControls `json:"controls"`
 
-	// Workflow
-	Workflow Workflow `json:"workflow"`
+	// Workflow contains the workflow that was detected or specified with the
+	// event.
+	Workflow *Workflow `json:"workflow"`
 }
 
+// ResultControls contains all the controls specified by the workflow that was
+// triggered. All controls must be fulfilled for the workflow run to be
+// fulfilled.
 type ResultControls struct {
 	Authentication *ResultControlsAuthentication `json:"authentication,omitempty"`
 	Reason         *ResultControlsReason         `json:"reason,omitempty"`
@@ -24,14 +28,28 @@ type ResultControls struct {
 }
 
 type ResultControlsAuthentication struct {
-	State  WorkflowState                     `json:"state"`
-	User   *ResultControlsAuthenticationUser `json:"user"`
-	URL    string                            `json:"url"`
-	ApiURL string                            `json:"api_url"`
+	// State contains the authentication request state.
+	State WorkflowState `json:"state"`
+
+	// User contains the user information if the user has authenticated
+	// successfully.
+	User *ResultControlsAuthenticationUser `json:"user"`
+
+	// URL contains the ephemeral URL for the user to authenticate with their
+	// Cased account for this particular workflow run. If running in a headless
+	// environment present the URL to the user to visit manually, otherwise
+	// redirect the user to the URL.
+	URL string `json:"url"`
+
+	// ApiURL contains the URL to check the status of the authentication request.
+	ApiURL string `json:"api_url"`
 }
 
 type ResultControlsAuthenticationUser struct {
-	ID    string `json:"id"`
+	// ID contains the authenticated Cased user ID.
+	ID string `json:"id"`
+
+	// Email contains the authenticated Cased user email address.
 	Email string `json:"email"`
 }
 
@@ -39,21 +57,49 @@ type ResultControlsReason struct {
 	State WorkflowState `json:"state"`
 }
 
+// ResultControlsApprovalState reflects the workflow approval state.
 type ResultControlsApprovalState string
 
 const (
-	ResultControlsApprovalStatePending   ResultControlsApprovalState = "pending"
+	// ResultControlsApprovalStatePending indicates when workflow controls have
+	// not yet been fulfilled to request approval from the configured approval
+	// sources.
+	//
+	// See that non-approval controls are fulfilled before the approval request
+	// will be requested.
+	ResultControlsApprovalStatePending ResultControlsApprovalState = "pending"
+
+	// ResultControlsApprovalStateRequested indicates the workflow has requested
+	// approval from the configured approval sources. The specified approval
+	// requirements have not been met.
 	ResultControlsApprovalStateRequested ResultControlsApprovalState = "requested"
-	ResultControlsApprovalStateApproved  ResultControlsApprovalState = "approved"
-	ResultControlsApprovalStateDenied    ResultControlsApprovalState = "denied"
-	ResultControlsApprovalStateTimedOut  ResultControlsApprovalState = "timed_out"
-	ResultControlsApprovalStateCanceled  ResultControlsApprovalState = "canceled"
+
+	// ResultControlsApprovalStateApproved indicates that the approval request
+	// has been approved per the configured approval requirements.
+	ResultControlsApprovalStateApproved ResultControlsApprovalState = "approved"
+
+	// ResultControlsApprovalStateDenied indicates that the approval request
+	// has been denied or did not meet approval requirements.
+	ResultControlsApprovalStateDenied ResultControlsApprovalState = "denied"
+
+	// ResultControlsApprovalStateTimedOut indicates that the approval request
+	// received no response within the configured timeout window.
+	ResultControlsApprovalStateTimedOut ResultControlsApprovalState = "timed_out"
+
+	// ResultControlsApprovalStateCanceled indicates that the approval request
+	// was canceled by the requester.
+	ResultControlsApprovalStateCanceled ResultControlsApprovalState = "canceled"
 )
 
 type ResultControlsApproval struct {
-	State    ResultControlsApprovalState     `json:"state"`
+	// State reflects the workflow result's approval state.
+	State ResultControlsApprovalState `json:"state"`
+
+	// Requests contains all approval requests sent. Feature is not yet enabled.
 	Requests []ResultControlsApprovalRequest `json:"requests"`
-	Source   ResultControlsApprovalSource    `json:"source"`
+
+	// Source contains the approval sources.
+	Source ResultControlsApprovalSource `json:"source"`
 }
 
 type ResultControlsApprovalRequestType string
@@ -65,10 +111,14 @@ type ResultControlsApprovalRequest struct {
 }
 
 type ResultControlsApprovalSource struct {
-	Email bool                              `json:"email"`
+	// Email indicates if the workflow approval request notified users via email.
+	Email bool `json:"email"`
+
+	// Slack indicates if the workflow approval request notified users via Slack.
 	Slack ResultControlsApprovalSourceSlack `json:"slack"`
 }
 
 type ResultControlsApprovalSourceSlack struct {
+	// Channel indicates the Slack channel the approval request was published to.
 	Channel string `json:"channel"`
 }
